164f5dcb4be7d33b89baab7b7b2de483a6e2e12a
danniwezz <danniwezz@hotmail.com>
2023-10-05 22:09:40 +0200

    .

--- CustomerModule/CustomerModule.Api/Database/Migrations/v0.03/01.AddWeekDaysToAssignedServiceTable.sql
+++ CustomerModule/CustomerModule.Api/Database/Migrations/v0.03/01.AddWeekDaysToAssignedServiceTable.sql
@@ -1,3 +1,5 @@
+ALTER TABLE [AssignedService] ADD [Price] decimal(18,4) NOT NULL
+ALTER TABLE [AssignedService] ADD [Currency] NVARCHAR(3) NOT NULL
 ALTER TABLE [AssignedService] ADD [StartDate] DATE NOT NULL
 ALTER TABLE [AssignedService] ADD [EndDate] DATE NULL
 ALTER TABLE [AssignedService] ADD [ValidToWeekDayNumber] INT NOT NULL
